37ebef2e9c655f1de74d1cb4c65e3808f527a60d
Nijii Daniels <danielsnijii@outlook.com>
2020-09-08 22:08:08 -0400

    My ASP.NET Beginner Exercises

---
+++ Exercises/Exercise07/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spot = new Places[36];
+            var place = new Places();
+
+            for (int i = 1; i <= 36; i++)
+            {
+                //var place = new Places();
+                double rowCounter = 3;
+                place.number = i;
+                place.color = i % 2 == 0 ? "Black" : "Red";
+                place.column = (int)Math.Ceiling((double)i / 3);
+                //place.row = (int)Math.Ceiling((double) i % 3);
+                place.row = (int) rowCounter;
+                if (i % 3 == 0)
+                {
+                    rowCounter--;
+                }
+             
+                Console.WriteLine($"{place.number} {place.color}, Column {place.column}, Row {rowCounter} ");
+            }
+            
+            Console.ReadLine();
+
+
+
+        }
+    }
+    public class Places
+    {
+        public int number { get; set; }
+
+        public string color { get; set; }
+
+        public int row { get; set; }
+
+        public int column { get; set; }
+
+
+    }
+}
---
+++ Exercises/Exercise08/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+
+namespace Exercise08
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numRange = { 1, 2, 3, 4, 5, 6, 8, 9, 10 };
+
+            List<int> numlist = new List<int>(numRange);
+            //double val1 = Math.Log2(10);
+
+            //Console.WriteLine(val1);
+
+            int enteredNum = Convert.ToInt32(Console.ReadLine());
+            bisectionMethod(enteredNum, numRange);
+            
+        }
+        public static bool bisectionMethod(int value, int[] sortednumRange)
+        {
+            //check the length
+
+            int middle = (int)sortednumRange.Length / 2;
+            var middleNumber = sortednumRange[middle];
+            if (middleNumber == value)
+            {
+                return true;
+            }
+
+            var halfofNumrange = new List<int>();
+            for (int j = 0; value <= middle; middle--)
+            {
+                
+                Console.WriteLine(value);
+            }
+            //return bisectionMethod(value, halfofNumrange);
+            return false;
+        }
+        //public static bool ContainsNumber(List<int> sortedInts, int value)
+        //{
+        //    //do something to split list into 2 lists
+        //    //use Console.WriteLine(); to print out your steps
+        //    //var lowerHalfOfInts
+        //    //var upperHalfOfInts
+
+        //    //check to see what list would contain the value
+
+        //    //make sure you have a way to break out of the recursion
+
+
+        //    //this is the recursion part
+        //    ContainsNumber(sortedInts, value);
+        //}
+
+    }
+}
